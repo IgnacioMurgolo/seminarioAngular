39b68f63bff0b7db0d000b815e66ca05ab1d3ce2
Lara Venere <laravenere2000@gmail.com>
2024-07-10 20:23:58 -0300

    agrega interfaz y template de accesorios-list

---
+++ src/app/accesorios-list/Accesorio.ts
@@ -0,0 +1,8 @@
+export interface Accesorio {
+    imagen : string,
+    descripcion : string, 
+    marca : string,
+    precio : number, 
+    stock : number,
+    oferta : boolean
+}
--- src/app/accesorios-list/accesorios-list.component.ts
+++ src/app/accesorios-list/accesorios-list.component.ts
@@ -1,18 +1,40 @@
 import { Component } from '@angular/core';
+import { Accesorio } from './Accesorio';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-accesorios-list',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './accesorios-list.component.html',
   styleUrl: './accesorios-list.component.scss'
 })
+
 export class AccesoriosListComponent {
-  accesorio = {
-    "imagen": "assets/img/guante-4stroke.jpeg",
-    "descripcion": "guante ripcurl",
-    "talle": "M",
-    "precio": 58000,
-    "stock": 6,
-  }
+  accesorios : Accesorio [] = [
+  {
+    imagen: "assets/img/guante-4stroke.jpeg",
+    descripcion: "guante invierno",
+    marca: "4Stroke",
+    precio: 58000,
+    stock: 6,
+    oferta: false,
+  },
+  {
+    imagen: "assets/img/guante-4stroke.jpeg",
+    descripcion: "guante verano",
+    marca: "ripcurl",
+    precio: 120000,
+    stock: 5,
+    oferta : true,
+  },
+  {
+    imagen: "assets/img/guante-4stroke.jpeg",
+    descripcion: "guante cuero",
+    marca: "royal enfield",
+    precio: 85000,
+    stock: 0,
+    oferta : false,
+  },
+]
 }
